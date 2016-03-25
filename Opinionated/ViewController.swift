16e7d0aa758c8194a1b1d5ab6bd9810b95fe3328
TheoRice <theo.rice1996@gmail.com>
2016-03-25 16:06:00 -0400

    initial commit w/ main screen and tables for articles

---
+++ Opinionated/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Opinionated
+//
+//  Created by Theodore Rice on 3/22/16.
+//  Copyright (c) 2016 opinionated. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    var repositories = [String]()
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return repositories.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = repositories[indexPath.row]
+        return cell
+    }
+
+}
+
